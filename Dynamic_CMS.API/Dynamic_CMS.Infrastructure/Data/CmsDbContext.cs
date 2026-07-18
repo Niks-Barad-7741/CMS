159a3e0d27bd5ec725bdf00c7316e9dac1be88d8
Aayan Halai <halaiayaan568@gmail.com>
2026-07-18 23:48:46 +0530

    Fix Bug Organigation Api

--- Dynamic_CMS.API/Dynamic_CMS.Infrastructure/Data/CmsDbContext.cs
+++ Dynamic_CMS.API/Dynamic_CMS.Infrastructure/Data/CmsDbContext.cs
@@ -45,7 +45,7 @@ namespace Dynamic_CMS.Infrastructure.Data
                 entity.HasKey(e => e.Id);
                 entity.Property(e => e.Name).IsRequired().HasMaxLength(200);
                 entity.Property(e => e.Slug).IsRequired().HasMaxLength(100);
-                entity.HasIndex(e => e.Slug).IsUnique();
+                entity.HasIndex(e => e.Slug);
             });
 
             modelBuilder.Entity<MenuItem>(entity =>
